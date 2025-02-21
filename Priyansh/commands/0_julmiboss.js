95166111347b0a1c9c16bef8787d2f4889934933
Shaan Personal bat <b8465391@gmail.com>
2025-02-21 23:15:55 +0500

    Update 0_julmiboss.js

--- Priyansh/commands/0_julmiboss.js
+++ Priyansh/commands/0_julmiboss.js
@@ -13,11 +13,11 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
   let react = event.body.toLowerCase();
   if(react.includes("Me boss hu teri") ||
-     react.includes("Teri malkin hu") || react.includes("Teri boss hu me") || react.includes("Teri malik hu me") ||
+     react.includes("Teri Malik hu") || react.includes("Teri boss hu me") || react.includes("Teri malik hu me") ||
 react.includes("Teri boss hu mai") ||
-react.includes("Malkin se battmizi")) {
+react.includes("Malik se battmizi")) {
     var msg = {
-        body: `【𝐒0𝐑𝐑𝐘 𝐌𝐀𝐋𝐊𝐈𝐍 𝐀𝐁 𝐆𝐀𝐋𝐓𝐈 𝐍𝐀𝐇𝐈 𝐊𝐀𝐑𝐔𝐍𝐆𝐀 𝐏𝐋𝐒 𝐀𝐊 𝐁𝐀𝐑 𝐌𝐀𝐅 𝐊𝐀𝐑 𝐃0______🤐😙😭😭🙏🙏】`,
+        body: `【𝐒0𝐑𝐑𝐘 𝐌𝐀𝐋𝐈𝐊 𝐀𝐁 𝐆𝐀𝐋𝐓𝐈 𝐍𝐀𝐇𝐈 𝐊𝐀𝐑𝐔𝐍𝐆𝐀 𝐏𝐋𝐒 𝐀𝐊 𝐁𝐀𝐑 𝐌𝐀𝐅 𝐊𝐀𝐑 𝐃0______🤐😙😭😭🙏🙏】`,
       }
       api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("👍", event.messageID, (err) => {}, true)
